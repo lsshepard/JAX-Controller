f4d31544f5721e5bca457f61e0e80b01ae079dcb
lsshepard <liam.robert.shepard@gmail.com>
2026-01-27 16:09:36 +0100

    FIX: bathtub plant robust to being empty

---
+++ Plant/Bathtub.py
@@ -0,0 +1,25 @@
+import jax.numpy as jnp
+from Plant.AbstractPlant import AbstractPlant
+
+class Bathtub(AbstractPlant):
+
+    def __init__(self, target_Y, min_D, max_D, A, C, dt=1):
+        self.A = A
+        self.C = C
+        super().__init__(target_Y, min_D, max_D, dt)
+
+    def get_V(self, H):
+        return jnp.sqrt(2*9.81*H + 1e-7)
+    
+    def get_init_state(self):
+        return self.get_target_Y()
+
+    def step(self, state, U, D):
+        Y = state
+        V = self.get_V(Y)
+        Q = V*self.C
+        dBdt = U + D - Q
+        dYdt = dBdt / self.A
+        dY = dYdt * self.dt
+        newY = jnp.clip(Y + dY, 0)
+        return (newY, newY)
